b50b402650aafe7b796545064a53420e9f266bb1
jeremy woo <kepungmoin@hotmail.com>
2019-10-22 13:24:05 +1300

    Create AppConstant.cs

---
+++ AppConstant.cs
@@ -0,0 +1,8 @@
+
+namespace LogProducer
+{
+    class AppConstant
+    {
+        public const string DATADOG_METRIC_SERIES_POST_URL = "https://api.datadoghq.com/api/v1/series?api_key=75cb6c3734c6525a829f8e0ca18ceaac";
+    }
+}
